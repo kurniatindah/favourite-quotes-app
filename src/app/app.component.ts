1208017f17b1b55db4f817daad74e0c17274a544
inf4my <naufal.irfan.h@gmail.com>
2017-11-16 08:04:38 +0700

    auth udah bisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { SignInPage } from '../pages/sign-in/sign-in';
 import { SignUpPage } from '../pages/sign-up/sign-up';
 import { SettingsPage } from '../pages/settings/settings';
+import { AuthService } from '../services/authService';
 
 @Component({
   templateUrl: 'app.html'
@@ -24,11 +25,16 @@ export class MyApp {
     platform: Platform,
     statusBar: StatusBar,
     splashScreen: SplashScreen,
-    private menuCtrl: MenuController) {
+    private menuCtrl: MenuController,
+    private authService: AuthService) {
 
     firebase.initializeApp({
       apiKey: "AIzaSyDUPTM8wuogtnphPOoOigH8AbUrBVqueaE",
-      authDomain: "favorite-quotes-app-45b18.firebaseapp.com"
+      authDomain: "favorite-quotes-app-45b18.firebaseapp.com",
+      databaseURL: "https://favorite-quotes-app-45b18.firebaseio.com",
+      projectId: "favorite-quotes-app-45b18",
+      storageBucket: "favorite-quotes-app-45b18.appspot.com",
+      messagingSenderId: "359971052923"
     })
 
     firebase.auth().onAuthStateChanged(user =>{
@@ -51,6 +57,10 @@ export class MyApp {
   onLoad(page: any) {
     this.nav.setRoot(page);
     this.menuCtrl.close();
+  }
+
+  logOut(){
+    this.authService.logout()
   }
 
 }
--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { NgForm } from "@angular/forms/src/forms";
+import { AuthService } from '../../services/authService';
 
 /**
  * Generated class for the SignInPage page.
@@ -14,11 +16,15 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SignInPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public authService: AuthService, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignInPage');
+  }
+
+  onSignIn(form: NgForm){
+    this.authService.signIn(form.value.email, form.value.password)
   }
 
 }
